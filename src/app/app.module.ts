c00cd6aba7676f21e1357f51577126522632b87f
Alexander Zent <azent@gmx.de>
2018-05-29 14:59:55 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { SwaggerService } from './swagger.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [SwaggerService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
